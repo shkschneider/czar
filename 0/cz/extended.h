5f13548a0558e886b1b629fbcd168b078112656f
ShkSchneider <shkschneider@noreply.codeberg.org>
2024-12-05 15:26:04 +0100

    std/ext libraries

---
+++ 0/cz/extended.h
@@ -0,0 +1,12 @@
+#ifndef CZAR_EXT_LIB_H_
+#define CZAR_EXT_LIB_H_
+
+#include <locale.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <unistd.h>
+
+# endif
---
+++ 0/cz/standard.h
@@ -0,0 +1,19 @@
+#ifndef CZAR_STD_LIB_H_
+#define CZAR_STD_LIB_H_
+
+#include <assert.h>
+#include <complex.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fenv.h>
+#include <math.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <threads.h>
+#include <time.h>
+
+# endif
