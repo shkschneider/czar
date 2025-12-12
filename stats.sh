2b37aacb166b46f88000ee2b8f9ca5640c6a010b
ShkSchneider <shkschneider@noreply.codeberg.org>
2025-12-12 12:12:10 +0100

    stats

--- stats.sh
+++ stats.sh
@@ -11,12 +11,6 @@ for f in *.lua *.sh ; do
         echo "- $m"
     done < <(grep "function $n" $f | cut -d' ' -f2- | cut -d'(' -f1 | tr ':' '.' | cut -d'.' -f2 | sort -u)
 done
-#git log --stat --summary HEAD lua-to-c \
-#    | grep '|' | cut -d'|' -f2 | grep -v Bin \
-#    | xargs \
-#    | sed -E 's; [\+\-]+ ;+;g' \
-#    | sed 's;0 ;;g' \
-#    | sed -E 's; \+\+\+;;g'
 echo "$r"
 
 # EOF
