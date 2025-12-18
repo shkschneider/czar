60e04dd1f80885391eaa8e14997adb4b0cc4e7dd
ShkSchneider <shkschneider@noreply.codeberg.org>
2025-12-18 23:16:58 +0100

    clean: improvements

--- clean.sh
+++ clean.sh
@@ -2,8 +2,12 @@
 
 rm -rf ./build
 
-find ./dist ./tests -type f \( \
-    -executable -o -name '*.h' -o -name '*.c' -o -name '*.o' -o -name '*.s' \
+find ./dist -type f \( \
+    -executable -o -name '*.h' -o -name '*.a' \
+\) -exec rm -vf -- {} +
+
+find ./tests -type f \( \
+    -executable -o -name '*.c' -o -name '*.s' \
 \) -exec rm -vf -- {} +
 
 rm -vf ./a.out ./cz
