ccf1c029b75862924923cb4b598e3d89f28487d6
ShkSchneider <shkschneider@noreply.codeberg.org>
2025-12-26 12:09:11 +0100

    std: version

--- src/codegen/init.lua
+++ src/codegen/init.lua
@@ -65,12 +65,13 @@ end
 -- Map module imports to their .cz file paths
 local function get_stdlib_file_path(import_path)
     local module_to_file = {
-        ["cz.os"] = "src/std/os.cz",
-        ["cz.fmt"] = "src/std/fmt.cz",
-        ["cz.alloc"] = "src/std/alloc/ialloc.cz",
-        ["cz.alloc.heap"] = "src/std/alloc/heap.cz",
-        ["cz.alloc.debug"] = "src/std/alloc/debug.cz",
         ["cz.alloc.arena"] = "src/std/alloc/arena.cz",
+        ["cz.alloc.debug"] = "src/std/alloc/debug.cz",
+        ["cz.alloc.heap"] = "src/std/alloc/heap.cz",
+        ["cz.alloc"] = "src/std/alloc/ialloc.cz",
+        ["cz.fmt"] = "src/std/fmt.cz",
+        ["cz.os"] = "src/std/os.cz",
+        ["cz.version"] = "src/std/version.cz",
     }
 
     return module_to_file[import_path]
