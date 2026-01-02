e20e63213a2b8ef695e3e9f652b7c8c422889b74
ShkSchneider <shkschneider@noreply.codeberg.org>
2026-01-02 16:55:59 +0100

    cleanup

--- src/transpiler.c
+++ src/transpiler.c
@@ -113,9 +113,9 @@ void transpiler_emit(Transpiler *transpiler, FILE *output) {
     if (!transpiler || !transpiler->ast || !output) {
         return;
     }
-    
+
     /* Inject runtime macro definitions at the beginning */
     fprintf(output, "%s", transpiler_get_runtime_macros());
-    
+
     emit_node(transpiler->ast, output);
 }
